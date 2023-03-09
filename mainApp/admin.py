a1032db86d4b1f20efea7e233344fa71128f2ea6
paulchirita <33900707+paulchirita@users.noreply.github.com>
2023-03-09 17:05:35 +0100

    Add basic REST Api.

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from mainApp.models import Article, NewsWebsite, Topic
+
 # Register your models here.
+admin.site.register(NewsWebsite)
+admin.site.register(Article)
+admin.site.register(Topic)
--- newsaggregator/urls.py
+++ newsaggregator/urls.py
@@ -14,10 +14,53 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from mainApp import views
+from rest_framework import routers, serializers, viewsets
+from mainApp.models import Article, NewsWebsite, Topic
+
+
+class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['title', 'newsWebsite', 'topic', 'articleText', 'date', 'nrLikes', 'nrSaves']
+
+
+class NewsWebsiteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = NewsWebsite
+        fields = ['name', 'url']
+
+
+class NewsWebsiteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Topic
+        fields = ['name']
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all().order_by('title')
+    serializer_class = ArticleSerializer
+
+
+class NewsWebsiteViewSet(viewsets.ModelViewSet):
+    queryset = NewsWebsite.objects.all().order_by('name')
+    serializer_class = NewsWebsiteSerializer
+
+
+class TopicWebsiteViewSet(viewsets.ModelViewSet):
+    queryset = Topic.objects.all().order_by('name')
+    serializer_class = NewsWebsiteSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'articles', ArticleViewSet)
+router.register(r'newsWebsites', NewsWebsiteViewSet)
+router.register(r'topics', TopicWebsiteViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.Index.as_view(), name='index')
+    path("", views.Index.as_view(), name='index'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
